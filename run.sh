18d7d6ca79447fc456434e8e4cef28d1f4d7f3d5
Kim Seonghyun <shyeon.kim@scipi.net>
2016-07-04 23:15:48 +0200

    Remove run.sh
    No point
    
    Signed-off-by: Kim Seonghyun <shyeon.kim@scipi.net>

--- run.sh
+++
@@ -1,6 +0,0 @@
-eval `/app/modules/0/bin/modulecmd bash add R/3.3.0`
-eval `/app/modules/0/bin/modulecmd bash add curl/7.45.0`
-eval `/app/modules/0/bin/modulecmd bash add gcc/5.3.0`
-eval `/app/modules/0/bin/modulecmd bash add boost/1.60`
-eval `/app/modules/0/bin/modulecmd bash add openssl/1.0.2`
-declare -x LD_LIBRARY_PATH="/app/vbuild/RHEL6-x86_64/openssl/1.0.2/lib/:/app/vbuild/RHEL6-x86_64/gcc/5.3.0/lib:/app/vbuild/RHEL6-x86_64/gcc/5.3.0/lib64/:/app/vbuild/RHEL6-x86_64/curl/7.27.0/lib:/app/vbuild/RHEL6-x86_64/openssl/1.0.2/lib/"
